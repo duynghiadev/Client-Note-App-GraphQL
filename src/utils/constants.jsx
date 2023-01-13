30cf651664208a6bf9b38075153439e7a9919c26
duynghia-123 <duynghia22302@gmail.com>
2023-01-13 11:55:03 +0700

    update client security

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -1,3 +1,3 @@
 export const GRAPHQL_SERVER = "https://note-app-server-eenm.onrender.com";
 export const GRAPHQL_SUBSCRIPTION_ENDPOINT =
-  "ws://note-app-server-eenm.onrender.com/";
+  "wss://note-app-server-eenm.onrender.com/";

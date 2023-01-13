a3b5f0541087da025794e6d7634976cbd7b07fb5
duynghia-123 <duynghia22302@gmail.com>
2023-01-13 11:51:35 +0700

    update server url

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -1,2 +1,3 @@
-export const GRAPHQL_SERVER = "http://localhost:4000";
-export const GRAPHQL_SUBSCRIPTION_ENDPOINT = "ws://localhost:4000/";
+export const GRAPHQL_SERVER = "https://note-app-server-eenm.onrender.com";
+export const GRAPHQL_SUBSCRIPTION_ENDPOINT =
+  "ws://note-app-server-eenm.onrender.com/";
